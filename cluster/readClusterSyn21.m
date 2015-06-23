a346ef71ce4373c4670b761f0a9e667dda1937b8
franrruiz <franrruiz@tsc.uc3m.es>
2015-06-23 09:31:57 +0200

    updated readClusterSyn21.m

--- cluster/readClusterSyn21.m
+++ cluster/readClusterSyn21.m
@@ -12,7 +12,7 @@ maxNt = 20;
 
 %%%%%%%%%%%%%%%
 plotToFile = 0;
-flagPlotGenie = 0;
+flagPlotGenie = 1;
 thrSER = 0.1;
 %%%%%%%%%%%%%%%
 
@@ -28,13 +28,14 @@ lHead = 0;
 onOffModel = 0;
 Nparticles = 3000;
 MAX_Nt = 10;
+EbN0cte = 0;
 
 % Sweep and hold variables:
   % L = 1:
   %sweep_var = 'SNR'; sweep_vec = -12:3:0; simId = 21; etiquetaX='-10log(\sigma_y^2)'; lugar='NorthEast'; hold_var='Nr'; hold_vec=20; subcarpeta = 'L1/';
   %sweep_var = 'Nt'; sweep_vec = 2:1:6; simId = 21; etiquetaX='#Transmitters'; lugar='NorthWest'; hold_var='SNR'; hold_vec=-3; subcarpeta = 'L1/';
   %sweep_var = 'Nr'; sweep_vec = [2:2:10 15:5:30]; simId = 21; etiquetaX='#Receivers'; lugar='NorthEast'; hold_var='SNR'; hold_vec=-3; subcarpeta = 'L1/';
-  sweep_var = 'M'; sweep_vec = 4:1:8; simId = 24; etiquetaX='log_2|A|'; lugar='SouthEast'; hold_var='SNR'; hold_vec=12; subcarpeta = 'L1/'; Niter=30000;
+  sweep_var = 'M'; sweep_vec = 4:2:10; simId = 24; etiquetaX='log_2|A|'; lugar='SouthEast'; hold_var='SNR'; hold_vec=14; subcarpeta = 'L1/'; Niter=30000; EbN0cte = 1;
 
   % L = 5:
   %sweep_var = 'SNR'; sweep_vec = -15:3:-6; simId = 21; etiquetaX='-10log(\sigma_y^2)'; lugar='NorthEast'; hold_var='Ltrue'; hold_vec=5; L=5; subcarpeta = 'L5/';
@@ -153,6 +154,24 @@ for sweepV=sweep_vec
             onOffModelaux = holdV;
         else
             error('Not valid value for hold_var')
+        end
+        
+        % Modify SNR if sweep M with EbN0=cte
+        if(EbN0cte)
+            if(strcmp(sweep_var,'M'))
+                SNRaux = SNRaux+10*log10(sweep_vec(idxSweep)/sweep_vec(1));
+                if(abs(SNRaux-14)<1e-3)
+                    SNRaux = 14;
+                elseif(abs(SNRaux-15.7609)<1e-3)
+                    SNRaux = 15.7609;
+                elseif(abs(SNRaux-17.0103)<1e-3)
+                    SNRaux = 17.0103;
+                elseif(abs(SNRaux-17.9794)<1e-3)
+                    SNRaux = 17.9794;
+                else
+                    error('Unexpected value found');
+                end
+            end
         end
         
         % Load all itCluster's for the given configuration
